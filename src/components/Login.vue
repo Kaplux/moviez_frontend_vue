505a4a6ba7bd3e4cd2b3f377765d6db37f683ee5
Alexandre Garbe <kaplux@gmail.com>
2018-01-09 23:11:24 +0100

    Added empty login component

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm4 offset-sm4>
+        <v-card raised>
+        <v-toolbar color="teal lighten-3" dark>
+          <v-toolbar-title>Login</v-toolbar-title>
+        </v-toolbar>         
+        <v-card-text>
+            <v-form>
+                <v-text-field
+                label="Email"
+                v-model="email"
+                ></v-text-field>
+                <v-text-field
+                label="Password"
+                v-model="password"
+                ></v-text-field>
+                <v-btn>
+                    submit
+                </v-btn>
+            </v-form>
+        </v-card-text>
+        </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+export default {
+  name: 'Login',
+  data: () => ({
+    email: '',
+    password: ''
+  })
+}
+</script>
