7ca74f2597635229f81c92f64709e982839bd39b
Alexandre Garbe <kaplux@gmail.com>
2018-01-09 23:10:43 +0100

    Added v-app to main component (for vuetifyjs)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <div id="app">
+      <img src="./assets/logo.png">
+      <router-view/>
+    </div>
+  </v-app>
 </template>
 
 <script>
